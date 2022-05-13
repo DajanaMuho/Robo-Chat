8ce34ef616d3c7210dfdb4a557e4124ce96927ab
dmuho <dmuho@airfind.com>
2022-05-13 00:17:32 -0400

    Remove unnessesary code

--- main.py
+++ main.py
@@ -1,36 +1,3 @@
-# used a dictionary to represent an intents JSON file
-data = {"intents": [
-             {"tag": "greeting",
-              "patterns": ["Hello", "How are you?", "Hi there", "Hi", "Whats up"],
-              "responses": ["Howdy Partner!", "Hello", "How are you doing?", "Greetings!", "How do you do?"],
-             },
-             {"tag": "age",
-              "patterns": ["how old are you?", "when is your birthday?", "when was you born?"],
-              "responses": ["I am 24 years old", "I was born in 1996", "My birthday is July 3rd and I was born in 1996", "03/07/1996"]
-             },
-             {"tag": "date",
-              "patterns": ["what are you doing this weekend?",
-"do you want to hang out some time?", "what are your plans for this week"],
-              "responses": ["I am available all week", "I don't have any plans", "I am not busy"]
-             },
-             {"tag": "name",
-              "patterns": ["what's your name?", "what are you called?", "who are you?"],
-              "responses": ["My name is Kippi", "I'm Kippi", "Kippi"]
-             },
-             {"tag": "goodbye",
-              "patterns": [ "bye", "g2g", "see ya", "adios", "cya"],
-              "responses": ["It was nice speaking to you", "See you later", "Speak soon!"]
-             }
-]}
-print("DUMB DATA", data)
-
-# ADDED - E. Miller
-# Testing to see if changes showing
-
-print("TEST _ EMILLER")
-
-
-
 ###################################################################################
 # E. Miller May 11, 2020 9:43 am
 
@@ -174,3 +141,5 @@ def text_preprocessor(text_df):
         # columns are filled
     
     return TXT
+
+
