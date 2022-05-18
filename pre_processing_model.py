3af525d834cdfb1f56a584f89d010de8354e8ddb
EdwardMMiller <91440219+EdwardMMiller@users.noreply.github.com>
2022-05-18 16:29:09 -0400

    added modified pre-processing function

--- pre_processing_model.py
+++ pre_processing_model.py
@@ -5,6 +5,8 @@ from nltk.stem import PorterStemmer
 import string
 import pandas as pd
 
+from robochat import lemmatizer
+
 nltk.download('omw-1.4')
 nltk.download("punkt")
 nltk.download("wordnet")
@@ -14,9 +16,6 @@ nltk.download("stopwords")
 
 # ADDING CONTRACTION EXPANDER - E MILLER
 import contractions
-
-lemmatizer = WordNetLemmatizer()
-
 
 def clean_text(text):
     tokens = nltk.word_tokenize(text)
@@ -97,76 +96,12 @@ class PreProcessing:
         # combining the two datasets
         display(dataset_FULL)
     """
-
-    #EDWARD PREPROCESSING CODE - CAUSES ERRORS, PLEASE CHECK
-""""
-    def text_preprocessor(text_df):
-
-        # this function takes in a data frame of text-only columns
-        # and goes through each column and processes the text data
-        # into a format that is more easily analyzed for prediction
-        # It then returns the converted text data frame
-        
-        num_rows = len(text_df)
-        num_cols = len(text_df.columns)
-        
-        # getting number of rows and columns in data frame
-        TXT = pd.DataFrame(columns = text_df.columns, index = text_df.index)
-        # setting up a new data frame to fill with processed text
-        punctuation = ",!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
-        # Saving a list of commonly used punctuation to a string in order to remove
-        # them from the text columns
-        stop_words = stopwords.words("english")
-        # getting list of common stopwords to remove from the text entries
-        lemma = WordNetLemmatizer()
-        # setting up lemmatizing function
-        stemm = PorterStemmer()
-        for i in range(num_cols):
-            # loop through the columns in the data frame
-            converted_text = []
-            # reset the converted text list so we can use it
-            # in the column
-            for j in range(num_rows):
-                # go through each row in the column
-                new_text = text_df.iloc[j][i].lower()           
-                # makes text lower case
-                # NOTE: i and j are reversed for this section,
-                # since we are going through each row in the column
-                for char in new_text:
-                    if char in punctuation:
-                        new_text = new_text.replace(char, " ")
-                # removing punctuation       
-                new_text = new_text.split()
-                # splitting up the text into individual words
-                for word in new_text:
-                    if word in stop_words:
-                        new_text.remove(word)
-                # removing stop words
-                for word in new_text:
-                    lemma.lemmatize(word)
-                # lemmatizing words
-                for word in new_text:
-                    stemm.stem(word)
-                # stemming words
-                new_text = " ".join(new_text)
-                converted_text.append(new_text)
-                # add the converted text value to the list
-            column_name = text_df.columns[i]
-            # get the name of the column that has been converted
-            TXT[ column_name ] = converted_text
-            # fill the rows of the corresponding column in TXT
-            # with the list of processed text and then return TXT after all
-            # columns are filled
-        
-        return TXT
-
-"""
-
-
 def text_preprocessor(text):
-    # this function takes in a string of text and processes it
 
-    punctuation = ",!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
+    # this function takes in a string of text and processes it
+    # so that it can more easily fit into the neural network
+
+    punctuation = ",!\#$%&()*+-./:;<=>?@[\]^_`{|}~'"
     # Saving a list of commonly used punctuation to a string in order to remove
     # them from the text columns
     stop_words = stopwords.words("english")
@@ -178,7 +113,7 @@ def text_preprocessor(text):
     # SETTING UP NEEDED FUNCTIONS TO PROCESS TEXT
 
     expanded_words = []
-    # creating an empty list
+    # creating an empty list for contractions
     for word in text.split():
         # using contractions.fix to expand the shortened words
         expanded_words.append(contractions.fix(word))
